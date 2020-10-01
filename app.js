61d2feb9f33bba37bc1a4742c5527b5479694933
Jeff Reeh <65681066+jareeh@users.noreply.github.com>
2020-09-30 21:16:12 -0500

    Add outline of app.js, set up data.json

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const data = require('./data.json');
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use('/static', express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+
+
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
